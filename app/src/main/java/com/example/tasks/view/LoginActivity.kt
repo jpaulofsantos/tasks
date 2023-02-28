c7d8827566fa3b924c54d62e2efc9e00e63dfb06
João Paulo Faustino Santos <jpaulofsantos@gmail.com>
2023-02-27 21:24:30 -0300

    login activity / login viewmodel

--- app/src/main/java/com/example/tasks/view/LoginActivity.kt
+++ app/src/main/java/com/example/tasks/view/LoginActivity.kt
@@ -2,11 +2,24 @@ package com.example.tasks.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.tasks.R
+import com.example.tasks.databinding.ActivityLoginBinding
+import com.example.tasks.viewmodel.LoginViewModel
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var viewModel: LoginViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
+
     }
 }
---
+++ app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.tasks.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class LoginViewModel(application: Application) : AndroidViewModel(application) {
+
+    fun login(email: String, pass: String) {
+
+    }
+
+    fun checkLogin() {
+
+    }
+
+}
